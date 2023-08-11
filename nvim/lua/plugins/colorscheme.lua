c76c962c8014ba3acf5286a52f962401bed05151
yaxin <yaxin.me@gmail.com>
2023-08-11 10:40:37 +0800

    change nvim colorschema

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- add gruvbox
+  { "folke/tokyonight.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+}
