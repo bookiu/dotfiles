8fc81bade87012fad9c9ee7c5fd507471efd4db8
yaxin <yaxin.me@gmail.com>
2023-12-14 16:20:15 +0800

    zsh: add kerberos function\nkitty: add kitty config

---
+++ zsh/zsh.d/kerberos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kinit_login() {
+    if [ -z "$KERBEROS_USER" -o -z "$KERBEROS_PASS" ]; then
+        echo "\$KERBEROS_USER or \$KERBEROS_PASS environment variables are not defined!"
+        return 1
+    fi
+    local cmd_line=$(
+        cat <<EOF
+spawn kinit $KERBEROS_USER
+expect {
+    "*assword*"  {
+        send "$KERBEROS_PASS\\n"
+    }
+}
+interact
+EOF
+    )
+
+    expect -c "$cmd_line"
+}
